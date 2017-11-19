1d7346110c1eaab1b76777427bc40520755ab901
AhmetK <ahmetburhan@gmail.com>
2017-11-18 22:56:54 -0800

    presudocode added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
+// Pseudocode of the Game
+// CSS done 
+// make list of the words
+// generateRandomWord
+// generate underscores for letters as mush as length
+// retrive key from player push to indexChecker
+// Change underscores to letter or increase wrong guess numbers
+// Win or lose condition indexChecker
+// Track totals
+// if won or lost start again with new game after alerting
+
+
+
+// ***PICKS A RANDOM WORD FROM ARRAY***
+function generateRandomWord() {
+    randomWord = words[Math.floor(Math.random() * (words.length))];
+}
+
+
 var words = ["ahmet", "mehmet", "apple"];
 var randomWord = "";
 var lives;
@@ -6,3 +25,17 @@ var guesses;
 var indexChecker;
 var gamesWon = 0;
 var donutBite = 0;
+
+// ***ADD UNDERSCORES BASED ON WORD LENGTH***
+function addSpaces(randomWord) {
+    for (var i = 0; i < randomWord.length; i++) {
+        if (randomWord[i] != " ") {
+            $("<span class='letters'>").text("_ ").appendTo("#chalk");
+        } else {
+            $("<span class='letters'>").html('&nbsp&nbsp&nbsp;').appendTo("#chalk");
+            winCounter++;
+        }
+    }
+}
+
+
